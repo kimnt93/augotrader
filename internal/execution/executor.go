28f9c996e0e34c541232901fc116694997d4049e
kim <kimnt93@gmail.com>
2024-07-08 13:59:25 +0700

    Update executor

--- internal/execution/executor.go
+++ internal/execution/executor.go
@@ -1,99 +1,121 @@
 package execution
 
 import (
-	"encoding/json"
-
 	"augotrader/internal/cache"
-	"augotrader/internal/notification"
+	"augotrader/internal/notibot"
 	"augotrader/internal/services"
 	"augotrader/internal/static"
-	"augotrader/internal/types"
+	"augotrader/internal/static/trading"
+	"math"
 
 	"github.com/rs/zerolog/log"
 )
 
-func executor(executeMeta types.ExecuteMeta) bool {
-	symbol := executeMeta.CurrentSigal.Symbol
-	accountId := executeMeta.AccountId
+func executeTrade(accountId string, symbol string) bool {
 
-	if services.IsLockedAccountSymbol(accountId, symbol) {
+	// Check if we cannot trade this symbol for this account
+	isLocked, err := services.IsLockedAccountSymbol(accountId, symbol)
+	if err != nil || isLocked {
+		log.Error().Msgf("Account %s is locked for symbol %s (locked=%b) or checking error: %v", accountId, symbol, isLocked, err)
 		return false
 	}
 
-	// Lock account and symbol before execute trade
-	services.LockAccountSymbol(accountId, symbol)
-
-	targetPosition := executeMeta.AccountConfig.TartgetPosition
-	targetOffset := executeMeta.AccountConfig.TargetOffset
-
-	currentPosition, err := services.GetCurrentPosition(accountId, symbol)
-	if err != nil {
-		log.Error().Msgf("Error while getting position (%s, %s): %v", accountId, symbol, err)
-		return false
-	} else {
-		// Calculate expected position based on weights
-		for strategyName, strategyValue := range executeMeta.Strategies {
-			log.Debug().Msgf("Strategy Name: %s, Weight: %f", strategyName, strategyValue.Weight, strategyValue.Name)
-			currentSignal := services.GetCurrentSignal(strategyValue.Name)
-
-			// Add volume
-			if currentSignal.Position == 0 {
-				// Skip, do nothing
-			} else {
-
-			}
-		}
-
-	}
-
-	executeQty := currentPosition - (targetPosition + targetOffset)
-	if executeQty == 0 {
-		// Do nothing
-	} else {
-		// Iterate over the Strategies map
-
-	}
-
-	// Release the lock
-	services.UnlockAccountSymbol(accountId, symbol)
-	return true
-}
-
-func executeTrade(accountId string, signal types.Signal) bool {
-
+	// Check if account is disabled
 	disabled, err := services.IsDisabledAccount(accountId)
 	if err != nil || disabled {
 		log.Error().Msgf("Account %s is disabled (disabled=%b) or checking error: %v", accountId, disabled, err)
 		return false
 	}
 
-	// current offset
-	currentOffset, err := services.GetCurrentOffset(accountId, signal.Symbol)
-	if err != nil {
-		log.Error().Msgf("Error getting current offset: %v", err)
-		return false
-	}
+	// Lock account and symbol before execute trade
+	services.LockAccountSymbol(accountId, symbol)
 
 	// current tradign strategy
-	tradingStrategy, err := services.GetTradingStrategyWeight(accountId)
+	tradingStrategies, err := services.GetTradingStrategyWeightByAccount(accountId)
 	if err != nil {
 		log.Error().Msgf("Error getting trading strategy: %v", err)
 		return false
 	}
 
-	var executeTradeMeta types.ExecuteMeta = types.ExecuteMeta{
-		AccountId:    accountId,
-		CurrentSigal: signal,
-		AccountConfig: types.AccountConfig{
-			TartgetPosition: signal.Position,
-			TargetOffset:    currentOffset,
-			AccountDisabled: disabled,
-		},
-		Strategies: tradingStrategy,
+	// Calculate expected position based on weights
+	positionWeight := 0.0
+	for strategyName, strategyValue := range tradingStrategies {
+		log.Debug().Msgf("Strategy Name: %s, Weight: %f", strategyName, strategyValue.Weight)
+
+		// Get current signal and calculate expected position
+		currentSignal, err := services.GetCurrentSignalByName(strategyValue.Name)
+		if err != nil {
+			log.Error().Msgf("Error getting current signal: %v", err)
+			return false
+		}
+		positionWeight += currentSignal.Position * strategyValue.Weight
 	}
 
-	// execute trade
-	return executor(executeTradeMeta)
+	// Get target position
+	targetPosition, err := services.GetTargetPosition(accountId, symbol)
+	if err != nil {
+		log.Error().Msgf("Error getting target position: %v", err)
+		return false
+	}
+
+	expectedPosition := positionWeight * targetPosition
+
+	// get current position
+	currentPosition, err := services.GetCurrentPosition(accountId, symbol)
+	if err != nil {
+		log.Error().Msgf("Error while getting position (%s, %s): %v", accountId, symbol, err)
+		return false
+	}
+
+	// get accouht current offset
+	currentOffset, err := services.GetCurrentOffset(accountId, symbol)
+	if err != nil {
+		log.Error().Msgf("Error getting current offset: %v", err)
+		return false
+	}
+
+	pendingOrders, err = services.GetSymbolPendingOrders(accountId, symbol)
+	if err != nil {
+		log.Error().Msgf("Error getting pending orders: %v", err)
+		return false
+	}
+
+	// loop over pending orders and calculate total pending quantity
+	totalPendingQty := 0.0
+	for _, order := range pendingOrders {
+		if order.Side == trading.BUY_SIDE {
+			totalPendingQty += order.Quantity
+		} else {
+			totalPendingQty -= order.Quantity
+		}
+	}
+
+	// Calculate the quantity to execute
+	// Remember checking total pending quantity to avoid over trade
+	// Round and convert by symbol
+	// 1. include 3 characters in upper case: round to lot 100
+	// 2. start with VN30: round to lot 1
+	// 3. other: float value
+	executeQty := expectedPosition - currentPosition + currentOffset - totalPendingQty
+	if executeQty == 0 {
+		log.Info().Msgf("No trade needed for account %s, symbol %s", accountId, symbol)
+		return true
+	}
+
+	// excute side B or S
+	executeSide := trading.BUY_SIDE
+	if executeQty < 0 {
+		executeSide = "S"
+		executeQty = math.Abs(executeQty)
+	}
+
+	// Execute trade
+	// Create/cancel, etc
+	services.CreateOrder(accountId, symbol, executeSide, executeQty)
+
+	// Release the lock for the next trade
+	services.UnlockAccountSymbol(accountId, symbol)
+	return true
 }
 
 // Run is the main function to execute the trade
@@ -109,23 +131,16 @@ func Run() {
 			log.Error().Msgf("Error getting all account ids: %v", err)
 			continue
 		}
-		msgPayload := msg.Payload
-		log.Info().Msgf("Received signal: %s", msgPayload)
-
-		// Unmarshal the JSON string into the Signal struct
-		var currentSignal types.Signal
-		if err := json.Unmarshal([]byte(msgPayload), &currentSignal); err != nil {
-			log.Error().Msgf("Error unmarshalling signal [%s]: %v", msgPayload, err)
-			continue
-		}
+		receivedActionSymbol := msg.Payload // HPG BTCUSDT VN30F2403
+		log.Info().Msgf("Received signal action symbol: %s", receivedActionSymbol)
 
 		// Loop all accounts and execute trade
 		for _, accountId := range allAccountids {
 			// Run in goroutine for each accountId
-			go executeTrade(accountId, currentSignal)
+			go executeTrade(accountId, receivedActionSymbol)
 		}
 
 		// Start send Signal to Telegram
-		notification.SendSignalToTelegram(currentSignal)
+		go notibot.SendSignalToTelegram("")
 	}
 }
--- internal/services/config.go
+++ internal/services/config.go
@@ -1,9 +1,23 @@
 package services
 
-func GetMaxPosition() int {
-	return 0
+import (
+	"augotrader/internal/cache"
+	"augotrader/internal/static"
+	"fmt"
+)
+
+func GetCurrentOffset(accountId string, symbol string) (float64, error) {
+	key := fmt.Sprintf("%s.%s.%s", static.CH_TARGET_OFFSET, accountId, symbol)
+	return cache.GetKeyFloat(key)
 }
 
-func GetAccountOffset(accountId string) int {
-	return 1
+func GetTargetPosition(accountId string, symbol string) (float64, error) {
+	key := fmt.Sprintf("%s.%s.%s", static.CH_TARGET_POSITION, accountId, symbol)
+	return cache.GetKeyFloat(key)
+}
+
+func IsDisabledAccount(accountId string) (bool, error) {
+	key := fmt.Sprintf("%s.%s", static.CH_ACCOUNT_DISABLED, accountId)
+	// Return the boolean value of the key
+	return cache.GetKeyBoolean(key)
 }
